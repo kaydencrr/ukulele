647c34a19945236bfb2030761b565d8b033326d6
kaydencrr <70279568+kaydencrr@users.noreply.github.com>
2020-08-26 22:28:26 +0700

    Uploaded All files

---
+++ Ukulele/Database.js
@@ -0,0 +1,419 @@
+database = 
+[
+	//A chord
+	{chord:"A", array: [0,2,1,0,0], pic: "chordA.jpg"},
+	{chord:"Am", array: [0,2,0,0,0], pic:"chordAm.jpg"},
+	{chord:"Aaug", array: [0,2,1,1,4], pic:"chordAaug.jpg"},
+	{chord:"Adim", array: [2,1,2,4,2], pic:"chordAdim.jpg"},
+	{chord:"A7", array: [0,0,1,0,0], pic:"chordA7.jpg"},
+	{chord:"Am7", array: [0,0,0,0,0], pic:"chordAm7.jpg"},
+	{chord:"Amaj7", array: [0,1,1,0,0], pic:"chordAmaj7.jpg"},
+	{chord:"Aaug7", array: [0,0,1,1,0], pic:"chordAaug7.jpg"},
+	{chord:"Adim7", array: [0,2,3,2,3], pic:"chordAdim7.jpg"},
+	{chord:"Am7b5", array: [0,2,3,3,3], pic:"chordAm7b5.jpg"},
+
+	//A# chord
+	{chord:"A#", array: [0,3,2,1,1], pic:"chordA#.jpg"},
+	{chord:"A#m", array: [0,3,1,1,1], pic:"chordA#m.jpg"},
+	{chord:"A#aug", array: [0,3,2,2,1], pic:"chordA#aug.jpg"},
+	{chord:"A#dim", array: [0,3,1,0,1], pic:"chordA#dim.jpg"},
+	{chord:"A#7", array: [0,1,2,1,1], pic:"chordA#7.jpg"},
+	{chord:"A#m7", array: [0,1,1,1,1], pic:"chordA#m7.jpg"},
+	{chord:"A#maj7", array: [0,3,2,1,0], pic:"chordA#maj7.jpg"},
+	{chord:"A#aug7", array: [0,1,2,2,1], pic:"chordA#aug7.jpg"},
+	{chord:"A#dim7", array: [0,0,1,0,1], pic:"chordA#dim7.jpg"},
+	{chord:"A#m7b5", array: [0,1,1,0,1], pic:"chordA#m7b5.jpg"},
+
+	//B chord
+	{chord:"B", array: [0,4,3,2,2], pic:"chordB.jpg"},
+	{chord:"Bm", array: [0,4,2,2,2], pic:"chordBm.jpg"},
+	{chord:"Baug", array: [0,4,3,3,2], pic:"chordBaug.jpg"},
+	{chord:"Bdim", array: [0,4,2,1,2], pic:"chordBdim.jpg"},
+	{chord:"B7", array: [0,2,3,2,2], pic:"chordB7.jpg"},
+	{chord:"Bm7", array: [0,2,2,2,2], pic:"chordBm7.jpg"},
+	{chord:"Bmaj7", array: [0,4,3,2,1], pic:"chordBmaj7.jpg"},
+	{chord:"Baug7", array: [0,4,3,3,0], pic:"chordBaug7.jpg"},
+	{chord:"Bdim7", array: [0,1,2,1,2], pic:"chordBdim7.jpg"},
+	{chord:"Bm7b5", array: [0,2,2,1,2], pic:"chordBm7b5.jpg"},
+
+	//C chord
+	{chord:"C", array: [0,0,0,0,3], pic: "chordC.jpg"},
+	{chord:"Cm", array: [0,0,3,3,3], pic: "chordCm.jpg"},
+	{chord:"Caug", array: [0,1,0,0,3], pic: "chordCaug.jpg"},
+	{chord:"Cdim", array: [0,0,3,2,3], pic: "chordCdim.jpg"},
+	{chord:"C7", array: [0,0,0,0,1], pic: "chordC7.jpg"},
+	{chord:"Cm7", array: [0,3,3,3,3], pic: "chordCm7.jpg"},
+	{chord:"Cmaj7", array: [0,0,0,0,2], pic: "chordCmaj7.jpg"},
+	{chord:"Caug7", array: [0,1,0,0,1], pic: "chordCaug7.jpg"},
+	{chord:"Cdim7", array: [0,2,3,2,3], pic: "chordCdim7.jpg"},
+	{chord:"Cm7b5", array: [0,3,3,2,3], pic: "chordCm7b5.jpg"},
+
+	//C# chord
+	{chord:"C#", array: [0,1,1,1,4], pic:"chordC#.jpg"},
+	{chord:"C#m", array: [0,1,1,0,4], pic:"chordC#m.jpg"},
+	{chord:"C#aug", array: [0,3,2,2,1], pic:"chordC#aug.jpg"},
+	{chord:"C#dim", array: [0,2,1,1,0], pic:"chordC#dim.jpg"},
+	{chord:"C#7", array: [0,0,1,0,4], pic:"chordC#7.jpg"},
+	{chord:"C#m7", array: [0,1,1,0,2], pic:"chordC#m7.jpg"},
+	{chord:"C#maj7", array: [0,1,1,1,3], pic:"chordC#maj7.jpg"},
+	{chord:"C#aug7", array: [0,2,1,1,2], pic:"chordC#aug7.jpg"},
+	{chord:"C#dim7", array: [0,0,1,0,1], pic:"chordC#dim7.jpg"},
+	{chord:"C#m7b5", array: [0,0,1,0,2], pic:"chordC#m7b5.jpg"},
+
+	//D chord
+	{chord:"D", array: [0,2,2,2,0]},
+	{chord:"Dm", array: [0,2,2,1,0]},
+	{chord:"Daug", array: [0,3,2,2,1]},
+	{chord:"Ddim", array: [0,1,2,1,0]},
+	{chord:"D7", array: [0,2,2,2,3]},
+	{chord:"Dm7", array: [0,2,2,1,3]},
+	{chord:"Dmaj7", array: [0,2,2,2,4]},
+	{chord:"Daug7", array: [0,1,2,1,2]},
+	{chord:"Ddim7", array: [0,1,2,1,2]},
+	{chord:"Dm7b5", array: [0,1,2,1,3]},
+
+	//D# chord
+	{chord:"D#", array: [0,0,3,3,1], pic:"chordA#.jpg"},
+	{chord:"D#m", array: [0,3,3,2,1]},
+	{chord:"D#aug", array: [0,0,3,3,2]},
+	{chord:"D#dim", array: [0,2,3,2,0]},
+	{chord:"D#7", array: [0,3,3,3,4]},
+	{chord:"D#m7", array: [0,3,3,2,4]},
+	{chord:"D#maj7", array: [2,2,2,2,4]},
+	{chord:"D#aug7", array: [0,4,3,3,4]},
+	{chord:"D#dim7", array: [0,2,3,2,3]},
+	{chord:"D#m7b5", array: [0,2,3,2,4]},
+	//E chord
+	{chord:"E", array: [0,1,4,0,2]},
+	{chord:"Em", array: [0,0,4,3,2]},
+	{chord:"Eaug", array: [0,1,0,0,3]},
+	{chord:"Edim", array: [0,0,4,0,1]},
+	{chord:"E7", array: [0,1,2,0,2]},
+	{chord:"Em7", array: [0,0,2,0,2]},
+	{chord:"Emaj7", array: [0,1,3,0,2]},
+	{chord:"Eaug7", array: [0,1,2,0,3]},
+	{chord:"Edim7", array: [0,0,1,0,1]},
+	{chord:"Em7b5", array: [0,0,2,0,1]},
+
+	//F chord
+	{chord:"F", array: [0,2,0,1,0]},
+	{chord:"Fm", array: [0,1,0,1,3]},
+	{chord:"Faug", array: [0,2,1,1,0]},
+	{chord:"Fdim", array: [0,1,0,1,2]},
+	{chord:"F7", array: [0,2,3,1,4]},
+	{chord:"Fm7", array: [0,1,3,1,3]},
+	{chord:"Fmaj7", array: [0,2,4,1,3]},
+	{chord:"Faug7", array: [0,2,3,1,4]},
+	{chord:"Fdim7", array: [0,1,2,1,2]},
+	{chord:"Fm7b5", array: [0,1,3,1,2]},
+
+	//F# chord
+	{chord:"F#", array: [0,3,1,2,1], pic:"chordA#.jpg"},
+	{chord:"F#m", array: [0,2,1,2,0]},
+	{chord:"F#aug", array: [0,3,2,2,1]},
+	{chord:"F#dim", array: [0,2,0,2,0]},
+	{chord:"F#7", array: [0,3,4,2,4]},
+	{chord:"F#m7", array: [0,2,4,2,4]},
+	{chord:"F#maj7", array: [2,2,4,1,3]},
+	{chord:"F#aug7", array: [3,1,4,0,3]},
+	{chord:"F#dim7", array: [0,2,3,2,3]},
+	{chord:"F#m7b5", array: [0,2,4,2,3]},
+
+	//G chord
+	{chord:"G", array: [0,0,2,3,2]},
+	{chord:"Gm", array: [0,0,2,3,1]},
+	{chord:"Gaug", array: [0,0,3,3,2]},
+	{chord:"Gdim", array: [0,0,1,3,1]},
+	{chord:"G7", array: [0,0,2,1,2]},
+	{chord:"Gm7", array: [0,0,2,1,1]},
+	{chord:"Gmaj7", array: [0,0,3,3,3]},
+	{chord:"Gaug7", array: [0,0,3,1,2]},
+	{chord:"Gdim7", array: [0,0,1,0,1]},
+	{chord:"Gm7b5", array: [0,0,1,1,1]},
+
+	//G# chord
+	{chord:"G#", array: [2,4,2,4,3], pic:"chordA#.jpg"},
+	{chord:"G#m", array: [0,4,3,4,2]},
+	{chord:"G#aug", array: [0,1,0,0,3]},
+	{chord:"G#dim", array: [0,1,2,0,2]},
+	{chord:"G#7", array: [0,1,3,2,3]},
+	{chord:"G#m7", array: [0,1,3,2,2]},
+	{chord:"G#maj7", array: [0,1,3,3,3]},
+	{chord:"G#aug7", array: [0,1,4,2,3]},
+	{chord:"G#dim7", array: [0,1,2,1,2]},
+	{chord:"G#m7b5", array: [0,1,2,2,2]},
+
+]
+
+database2 = 
+[
+	
+	{rootNote: "A", chords:[
+		{chord:"A", array: [0,2,1,0,0]},
+		{chord:"Am", array: [0,2,0,0,0]},
+		{chord:"Aaug", array: [0,2,1,1,4]},
+		{chord:"Adim", array: [2,1,2,4,2]},
+		{chord:"A7", array: [0,0,1,0,0]},
+		{chord:"Am7", array: [0,0,0,0,0]},
+		{chord:"Amaj7", array: [0,1,1,0,0]},
+		{chord:"Aaug7", array: [0,0,1,1,0]},
+		{chord:"Adim7", array: [0,2,3,2,3]},
+		{chord:"Am7b5", array: [0,2,3,3,3]},
+	]},
+
+	{rootNote: "A#", chords:[
+		{chord:"A#", array: [0,3,2,1,1]},
+		{chord:"A#m", array: [0,3,1,1,1]},
+		{chord:"A#aug", array: [0,3,2,2,1]},
+		{chord:"A#dim", array: [0,3,1,0,1]},
+		{chord:"A#7", array: [0,1,2,1,1]},
+		{chord:"A#m7", array: [0,1,1,1,1]},
+		{chord:"A#maj7", array: [0,3,2,1,0]},
+		{chord:"A#aug7", array: [0,1,2,2,1]},
+		{chord:"A#dim7", array: [0,0,1,0,1]},
+		{chord:"A#m7b5", array: [0,1,1,0,1]},
+	]},
+
+	{rootNote: "B", chords:[
+		{chord:"B", array: [0,4,3,2,2]},
+	{chord:"Bm", array: [0,4,2,2,2]},
+	{chord:"Baug", array: [0,4,3,3,2]},
+	{chord:"Bdim", array: [0,4,2,1,2]},
+	{chord:"B7", array: [0,2,3,2,2]},
+	{chord:"Bm7", array: [0,2,2,2,2]},
+	{chord:"Bmaj7", array: [0,4,3,2,1]},
+	{chord:"Baug7", array: [0,4,3,3,0]},
+	{chord:"Bdim7", array: [0,1,2,1,2]},
+	{chord:"Bm7b5", array: [0,2,2,1,2]},
+	]},
+
+	{rootNote: "C", chords:[
+		{chord:"C", array: [0,0,0,0,3]},
+		{chord:"Cm", array: [0,0,3,3,3]},
+		{chord:"Caug", array: [0,1,0,0,3]},
+		{chord:"Cdim", array: [0,0,3,2,3]},
+		{chord:"C7", array: [0,0,0,0,1]},
+		{chord:"Cm7", array: [0,3,3,3,3]},
+		{chord:"Cmaj7", array: [0,0,0,0,2]},
+		{chord:"Caug7", array: [0,1,0,0,1]},
+		{chord:"Cdim7", array: [0,2,3,2,3]},
+		{chord:"Cm7b5", array: [0,3,3,2,3]},
+	]},
+
+	{rootNote: "C#", chords:[
+		{chord:"C#", array: [0,1,1,1,4]},
+		{chord:"C#m", array: [0,1,1,0,4]},
+		{chord:"C#aug", array: [0,3,2,2,1]},
+		{chord:"C#dim", array: [0,2,1,1,0]},
+		{chord:"C#7", array: [0,0,1,0,4]},
+		{chord:"C#m7", array: [0,1,1,0,2]},
+		{chord:"C#maj7", array: [0,1,1,1,3]},
+		{chord:"C#aug7", array: [0,2,1,1,2]},
+		{chord:"C#dim7", array: [0,0,1,0,1]},
+		{chord:"C#m7b5", array: [0,0,1,0,2]},
+	]},
+
+	{rootNote: "D", chords:[
+		{chord:"D", array: [0,2,2,2,0]},
+		{chord:"Dm", array: [0,2,2,1,0]},
+		{chord:"Daug", array: [0,3,2,2,1]},
+		{chord:"Ddim", array: [0,1,2,1,0]},
+		{chord:"D7", array: [0,2,2,2,3]},
+		{chord:"Dm7", array: [0,2,2,1,3]},
+		{chord:"Dmaj7", array: [0,2,2,2,4]},
+		{chord:"Daug7", array: [0,1,2,1,2]},
+		{chord:"Ddim7", array: [0,1,2,1,2]},
+		{chord:"Dm7b5", array: [0,1,2,1,3]},
+	]},
+
+	{rootNote: "D#", chords:[
+		{chord:"D#", array: [0,0,3,3,1]},
+		{chord:"D#m", array: [0,3,3,2,1]},
+		{chord:"D#aug", array: [0,0,3,3,2]},
+		{chord:"D#dim", array: [0,2,3,2,0]},
+		{chord:"D#7", array: [0,3,3,3,4]},
+		{chord:"D#m7", array: [0,3,3,2,4]},
+		{chord:"D#maj7", array: [2,2,2,2,4]},
+		{chord:"D#aug7", array: [0,4,3,3,4]},
+		{chord:"D#dim7", array: [0,2,3,2,3]},
+		{chord:"D#m7b5", array: [0,2,3,2,4]},
+	]},
+
+	{rootNote: "E", chords:[
+		{chord:"E", array: [0,1,4,0,2]},
+		{chord:"Em", array: [0,0,4,3,2]},
+		{chord:"Eaug", array: [0,1,0,0,3]},
+		{chord:"Edim", array: [0,0,4,0,1]},
+		{chord:"E7", array: [0,1,2,0,2]},
+		{chord:"Em7", array: [0,0,2,0,2]},
+		{chord:"Emaj7", array: [0,1,3,0,2]},
+		{chord:"Eaug7", array: [0,1,2,0,3]},
+		{chord:"Edim7", array: [0,0,1,0,1]},
+		{chord:"Em7b5", array: [0,0,2,0,1]},	
+	]},
+
+	{rootNote: "F", chords:[
+		{chord:"F", array: [0,2,0,1,0]},
+		{chord:"Fm", array: [0,1,0,1,3]},
+		{chord:"Faug", array: [0,2,1,1,0]},
+		{chord:"Fdim", array: [0,1,0,1,2]},
+		{chord:"F7", array: [0,2,3,1,4]},
+		{chord:"Fm7", array: [0,1,3,1,3]},
+		{chord:"Fmaj7", array: [0,2,4,1,3]},
+		{chord:"Faug7", array: [0,2,3,1,4]},
+		{chord:"Fdim7", array: [0,1,2,1,2]},
+		{chord:"Fm7b5", array: [0,1,3,1,2]},
+	]},
+
+	{rootNote: "F#", chords:[
+		{chord:"F#", array: [0,3,1,2,1]},
+		{chord:"F#m", array: [0,2,1,2,0]},
+		{chord:"F#aug", array: [0,3,2,2,1]},
+		{chord:"F#dim", array: [0,2,0,2,0]},
+		{chord:"F#7", array: [0,3,4,2,4]},
+		{chord:"F#m7", array: [0,2,4,2,4]},
+		{chord:"F#maj7", array: [2,2,4,1,3]},
+		{chord:"F#aug7", array: [3,1,4,0,3]},
+		{chord:"F#dim7", array: [0,2,3,2,3]},
+		{chord:"F#m7b5", array: [0,2,4,2,3]},
+	]},
+
+	{rootNote: "G", chords:[
+		{chord:"G", array: [0,0,2,3,2]},
+		{chord:"Gm", array: [0,0,2,3,1]},
+		{chord:"Gaug", array: [0,0,3,3,2]},
+		{chord:"Gdim", array: [0,0,1,3,1]},
+		{chord:"G7", array: [0,0,2,1,2]},
+		{chord:"Gm7", array: [0,0,2,1,1]},
+		{chord:"Gmaj7", array: [0,0,3,3,3]},
+		{chord:"Gaug7", array: [0,0,3,1,2]},
+		{chord:"Gdim7", array: [0,0,1,0,1]},
+		{chord:"Gm7b5", array: [0,0,1,1,1]}
+	]},
+
+	{rootNote: "G#", chords:[
+		{chord:"G#", array: [2,4,2,4,3]},
+		{chord:"G#m", array: [0,4,3,4,2]},
+		{chord:"G#aug", array: [0,1,0,0,3]},
+		{chord:"G#dim", array: [0,1,2,0,2]},
+		{chord:"G#7", array: [0,1,3,2,3]},
+		{chord:"G#m7", array: [0,1,3,2,2]},
+		{chord:"G#maj7", array: [0,1,3,3,3]},
+		{chord:"G#aug7", array: [0,1,4,2,3]},
+		{chord:"G#dim7", array: [0,1,2,1,2]},
+		{chord:"G#m7b5", array: [0,1,2,2,2]} 
+	]},
+]
+
+
+var rootNoteA = document.getElementById("rootA");
+var rootNoteAs = document.getElementById("rootA#");
+var rootNoteB = document.getElementById("rootB");
+var rootNoteC = document.getElementById("rootC");
+var rootNoteCs = document.getElementById("rootC#");
+var rootNoteD = document.getElementById("rootD");
+var rootNoteDs = document.getElementById("rootD#");
+var rootNoteE = document.getElementById("rootE");
+var rootNoteF = document.getElementById("rootF");
+var rootNoteFs = document.getElementById("rootF#");
+var rootNoteG = document.getElementById("rootG");
+var rootNoteGs = document.getElementById("rootG#");
+
+var resultDia = document.getElementById("resultDiagram");
+
+function toggle(){
+	//var a = document.getElementById("showA");
+	var as = document.getElementById("showA#");
+	var b = document.getElementById("showB");
+	var c = document.getElementById("showC");
+	var cs = document.getElementById("showC#");
+	var d = document.getElementById("showD");
+	
+	//a.style.display = rootNoteA.checked ? "block" : "none";
+	as.style.display = rootNoteAs.checked ? "block" : "none";
+	b.style.display = rootNoteB.checked ? "block" : "none";
+	c.style.display = rootNoteC.checked ? "block" : "none";
+	cs.style.display = rootNoteCs.checked ? "block" : "none";
+	d.style.display = rootNoteD.checked ? "block" : "none";
+	
+}
+
+/*
+rootNoteA.addEventListener("click", function(){
+	inputRootNote = "A";
+	var x = document.getElementById("showA");
+	if(x.style.display === "block"){
+		x.style.dysplay = "none";
+	}else{
+		x.style.display = "none";
+	}
+	console.log(searchRootNote());
+});
+*/
+rootNoteAs.addEventListener("click", function(){
+	inputRootNote = "A#";
+	console.log(searchRootNote());
+});
+rootNoteB.addEventListener("click", function(){
+	inputRootNote = "B";
+	console.log(searchRootNote());
+})
+rootNoteC.addEventListener("click", function(){
+	inputRootNote = "C";
+	console.log(searchRootNote());
+
+});
+rootNoteCs.addEventListener("click", function(){
+	inputRootNote = "C#";
+	console.log(searchRootNote());
+});
+rootNoteD.addEventListener("click", function(){
+	inputRootNote = "D";
+	console.log(searchRootNote());
+});
+rootNoteDs.addEventListener("click", function(){
+	inputRootNote = "D#";
+	console.log(searchRootNote());
+});
+rootNoteE.addEventListener("click", function(){
+	inputRootNote = "E";
+	console.log(searchRootNote());
+});
+rootNoteF.addEventListener("click", function(){
+	inputRootNote = "F";
+	console.log(searchRootNote());
+});
+rootNoteFs.addEventListener("click", function(){
+	inputRootNote = "F#";
+	console.log(searchRootNote());
+});
+rootNoteG.addEventListener("click", function(){
+	inputRootNote = "G";
+	console.log(searchRootNote());
+});
+rootNoteGs.addEventListener("click", function(){
+	inputRootNote = "G#";
+	console.log(searchRootNote());
+});
+
+
+const searchRootNote = () => database2.forEach(pairofChord => {
+	if (inputRootNote == pairofChord.rootNote) {
+		console.log(pairofChord.chords);
+	}
+});
+
+ //search for diagram
+const searchChord = () => database.forEach(pairofChord => {
+	if (inputChord == pairofChord.chord){
+		document.getElementById("resultDiagram").src = pairofChord.pic;
+		console.log(pairofChord.array);
+	}
+});
+
+//search for name
+const searchArray = () => database.forEach(pairofChord => {
+	if(JSON.stringify(inputChord)==JSON.stringify(pairofChord.array)){
+		console.log(pairofChord.chord);
+	}	
+})
